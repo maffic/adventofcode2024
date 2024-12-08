e5afbfa684002dd1235a1162e33ec065eba86927
Maksim <mafficdba@gmail.com>
2024-12-08 18:08:26 +0300

    day 7 + day 8

---
+++ test/src/day7/Task7.java
@@ -0,0 +1,111 @@
+package day7;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.stream;
+
+public class Task7 {
+
+    private static final String FILE_PATH = "D:\\Project\\adventofcode2024\\test\\src\\day7\\input.file";
+    private List<String> lines;
+
+    private void loadFile() {
+        try (Stream<String> linesFile = Files.lines(Paths.get(FILE_PATH))) {
+            lines = linesFile.toList();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void findResult() {
+        long sum = 0;
+        long sumConcated = 0;
+        int number = 0;
+
+        for (String line : lines) {
+            String[] split = line.split(":");
+
+            long target = Long.parseLong(split[0]);
+            long[] values = stream(split[1].trim().split(" ")).mapToLong(Long::parseLong).toArray();
+            if (dfs(number, target, values, 1, values[0], String.valueOf(values[0]))) {
+                sum += target;
+            } else {
+                if (dfsConcate(target, values, 1, values[0], String.valueOf(values[0]))) {
+                    sumConcated += target;
+                } else {
+                    System.out.println(number + ": Не найдено выражение для " + target + " в строке " + line);
+                }
+            }
+            number++;
+        }
+
+        System.out.println("Result part1 = " + sum);
+        System.out.println("Result part2 = " + sumConcated);
+        System.out.printf("Result = %d%n", sumConcated + sum);
+    }
+
+    private boolean dfs(int number, Long target, long[] value, int index, long currentSum, String expression) {
+        if (index == value.length) {
+            if (currentSum == target) {
+                System.out.println(number + ": Найдено выражение: " + expression + " = " + target + " (" + currentSum + ")");
+                return true;
+            }
+            return false;
+        }
+
+        return dfs(number, target, value, index + 1, currentSum + value[index], expression + " + " + value[index])
+                || dfs(number, target, value, index + 1, currentSum * value[index], expression + " * " + value[index]);
+    }
+
+    private boolean dfsConcate(long target, long[] numbers, int index, long currentResult, String expression) {
+        if (index == numbers.length) {
+            if (currentResult == target) {
+                return true;
+            }
+            return false;
+        }
+
+        long nextNumber = numbers[index];
+
+        // +
+        if (dfsConcate(target, numbers, index + 1, currentResult + nextNumber,
+                expression + " + " + nextNumber)) {
+            return true;
+        }
+
+        // *
+        if (dfsConcate(target, numbers, index + 1, currentResult * nextNumber,
+                expression + " * " + nextNumber)) {
+            return true;
+        }
+
+        // текущего результата и следующее число
+        String concatenatedStr = expression + nextNumber;
+        long concatenatedResult = Long.parseLong(concatenatedStr);
+        if (dfsConcate(target, numbers, index + 1, concatenatedResult, concatenatedStr)) {
+            return true;
+        }
+
+
+        // результат и результат
+        String combinedConcat = currentResult + String.valueOf(nextNumber);
+        long combinedConcatResult = Long.parseLong(combinedConcat);
+        String combinedConcatExpression = expression + nextNumber;
+        if (dfsConcate(target, numbers, index + 1, combinedConcatResult, combinedConcatExpression)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        var task = new Task7();
+        task.loadFile();
+        task.findResult();
+    }
+
+}
---
+++ test/src/day8/Task8.java
@@ -0,0 +1,124 @@
+package day8;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class Task8 {
+
+    private static final String FILE_PATH = "D:\\Project\\adventofcode2024\\test\\src\\day8\\input.file";
+    private List<String> map;
+
+
+    private void loadData() {
+        try (Stream<String> linesFile = Files.lines(Paths.get(FILE_PATH))) {
+            map = linesFile.toList();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void countAntinodes() {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < map.size(); i++) {
+            char[] chars = map.get(i).toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j] != '.') {
+//                    searchAntinodes(set, chars[j], i, j);
+                    String add = i + " " + j;
+                    set.add(add);
+                    searchAntinodesInfinit(set, chars[j], i, j);
+                }
+            }
+
+        }
+
+        printResult(set);
+
+        System.out.println("Part 1 count : " + set.size());
+    }
+
+    private void printResult(Set<String> set) {
+        for (int i = 0; i < map.size(); i++) {
+            char[] chars = map.get(i).toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j] == '.') {
+                    if (set.contains(i + " " + j)) {
+                        System.out.print("#");
+                        continue;
+                    }
+                } else {
+                    if (set.contains(i + " " + j)) {
+                        System.out.print("*");
+                        continue;
+                    }
+                }
+                System.out.print(chars[j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private void searchAntinodes(Set<String> set, char searchChar, int i, int j) {
+        for (int x = i; x < map.size(); x++) {
+            String search = map.get(x);
+            int y = search.indexOf(searchChar);
+            while (y != -1) {
+                if (x != i || y != j) {
+                    int dx = x - i;
+                    int dy = y - j;
+                    int[] up = new int[] {i - dx, j - dy};
+                    int[] down = new int[] {x + dx, y + dy};
+                    checkOutOfBounds(up, set);
+                    checkOutOfBounds(down, set);
+                }
+                y = search.indexOf(searchChar, y + 1);
+            }
+        }
+    }
+
+    private void searchAntinodesInfinit(Set<String> set, char searchChar, int i, int j) {
+        for (int x = i; x < map.size(); x++) {
+            String search = map.get(x);
+            int y = search.indexOf(searchChar);
+            while (y != -1) {
+                if (x != i || y != j) {
+                    int dx = x - i;
+                    int dy = y - j;
+                    int[] up = new int[] {i, j};
+                    int[] down = new int[] {x, y};
+                    do {
+                        up[0] = up[0] - dx;
+                        up[1] = up[1] - dy;
+                        down[0] = down[0] + dx;
+                        down[1] = down[1] + dy;
+                        checkOutOfBounds(up, set);
+                        checkOutOfBounds(down, set);
+                    } while (outOfBounds(down) || outOfBounds(up));
+                }
+                y = search.indexOf(searchChar, y + 1);
+            }
+        }
+    }
+
+    private void checkOutOfBounds(int[] check, Set<String> set) {
+        if (outOfBounds(check)) {
+            String add = check[0] + " " + check[1];
+            set.add(add);
+        }
+    }
+
+    private boolean outOfBounds(int[] check) {
+        return check[0] >= 0 && check[0] < map.size() && check[1] >= 0 && check[1] < map.get(check[0]).length();
+    }
+
+    public static void main(String[] args) {
+        var task = new Task8();
+        task.loadData();
+        task.countAntinodes();
+    }
+}
